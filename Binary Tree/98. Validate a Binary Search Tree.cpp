0ed7c38d725e2427826ef97c63879f9628e062c8
Jahin Mahruf <jmsmahruf@gmail.com>
2026-04-08 12:28:24 +0600

    Implement 98. validation for a Binary Search Tree

--- Binary Tree/98. Validate a Binary Search Tree.cpp
+++ Binary Tree/98. Validate a Binary Search Tree.cpp
@@ -1,1 +1,20 @@
+class Solution {
+public:
+    void inOrder(TreeNode* node, vector<int> &v) {
+        if(!node) return;
+        inOrder(node->left, v);
+        v.push_back(node->val);
+        inOrder(node->right, v);
+    }
 
+    bool isValidBST(TreeNode* root) {
+        vector<int> v;
+        inOrder(root, v);
+
+        for(int i = 1; i < v.size(); i++) {
+            if(v[i] <= v[i-1]) return false;
+        }
+
+        return true;
+    }
+};
