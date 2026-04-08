3b5c955c33930eb08392cf4d3fae2539b624844b
Jahin Mahruf <jmsmahruf@gmail.com>
2026-04-08 11:29:29 +0600

    Implement 69. binary search for square root calculation

--- Binary Search/69. Sqrt(x).cpp
+++ Binary Search/69. Sqrt(x).cpp
@@ -1,1 +1,18 @@
-
+class Solution {
+public:
+    int mySqrt(int x) {
+        long low = 0, high = x;
+        int ans = 0;
+        while(low <= high)
+        {
+            long mid = (low + high)/2;
+            if(mid * mid > x)high = mid - 1;
+            else
+            {
+                ans = mid;
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
+};
