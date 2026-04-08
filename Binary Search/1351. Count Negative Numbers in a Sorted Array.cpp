a78acce6b350111078f76f5c4ac01d23b9b17097
Jahin Mahruf <jmsmahruf@gmail.com>
2026-04-08 11:56:43 +0600

    Add 1351. countNegatives function to count negative numbers
    Implement countNegatives function to count negative numbers in a sorted 2D array.

--- Binary Search/1351. Count Negative Numbers in a Sorted Array.cpp
+++ Binary Search/1351. Count Negative Numbers in a Sorted Array.cpp
@@ -1,1 +1,12 @@
-
+class Solution {
+public:
+    int countNegatives(vector<vector<int>>& grid) {
+        int cnt = 0;
+        for(int i = 0; i < grid.size(); i++)
+        {
+            int ind = upper_bound(grid[i].begin(), grid[i].end(), 0, greater<int>()) - grid[i].begin();
+            cnt += grid[i].size() - ind;
+        }
+        return cnt;
+    }
+};
